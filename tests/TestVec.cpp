b6eac951b6d22fa5ce1a486dc45fdb7c910b9555
Oscar Shiang <oscar0225@livemail.tw>
2024-11-07 09:28:57 +0800

    Remove prefix or postfix of each test item

--- tests/TestVec.cpp
+++ tests/TestVec.cpp
@@ -3,13 +3,13 @@
 
 #include "vec.h"
 
-TEST(TestVec, SizeTest) {
+TEST(Vec, Size) {
     picola::vec<3> v1;
 
     EXPECT_EQ(v1.size(), 3);
 }
 
-TEST(TestVec, OStreamTest) {
+TEST(Vec, OStream) {
     picola::vec<5> v1;
 
     v1(0) = 1;
@@ -24,7 +24,7 @@ TEST(TestVec, OStreamTest) {
     EXPECT_EQ(ss.str(), "1 2 3 4 5");
 }
 
-TEST(TestVec, AddTest) {
+TEST(Vec, Add) {
     picola::vec<3> v1({5, 6, 7});
     picola::vec<3> v2({6, 7, 8});
 
@@ -44,7 +44,7 @@ TEST(TestVec, AddTest) {
     EXPECT_EQ(ss.str(), "14.5 16.5 18.5");
 }
 
-TEST(TestVec, SubTest) {
+TEST(Vec, Sub) {
     picola::vec<3> v1({1.2, 1.3, 1.4});
     picola::vec<3> v2({1, 1, 1});
 
@@ -64,7 +64,7 @@ TEST(TestVec, SubTest) {
     EXPECT_EQ(ss.str(), "-10 -9.9 -9.8");
 }
 
-TEST(TestVec, MulTest) {
+TEST(Vec, Mul) {
     picola::vec<3> v1({5, 4, 3});
     picola::vec<3> v2({2, 3, 4});
 
@@ -83,7 +83,7 @@ TEST(TestVec, MulTest) {
     EXPECT_EQ(ss.str(), "5 6 6");
 }
 
-TEST(TestVec, DivTest) {
+TEST(Vec, Div) {
     picola::vec<3> v1({100, 200, 300});
 
     v1 = v1 / 100;
