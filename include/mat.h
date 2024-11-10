62911628c606eaaeda918d885cc806ad08fe1f11
Oscar Shiang <oscar0225@livemail.tw>
2024-11-10 20:21:26 +0800

    Add matrix initializer and access functions

--- include/mat.h
+++ include/mat.h
@@ -14,21 +14,6 @@ public:
         memset(m_buf, 0, sizeof(float) * R * C);
     }
 
-    mat(std::vector<std::vector<float> > &m)
-    {
-        // FIXME: This function is only useful for unit-testing.
-        //        Could be useless in practice (No std::vector for embedded devices)
-        if (m_nrow != m.size() || m_ncol != m[0].size()) {
-            throw std::invalid_argument("Dimension mismatch");
-        }
-
-        // Assume to follow row major at the begining
-        for (size_t i = 0; i < m.size(); i++) {
-            memcpy(&m_buf[i * m_nrow], m[i].data(),
-                   sizeof(float) * m[i].size());
-        }
-    }
-
     size_t nrow()
     {
         return m_nrow;
@@ -44,7 +29,60 @@ public:
         return m_nelem;
     }
 
+    float &operator()(size_t row, size_t col)
+    {
+        if (row >= m_nrow || col >= m_ncol) {
+            throw std::invalid_argument("Dimension mismatch");
+        }
+        return m_buf[index(row, col)];
+    }
+
+    float operator()(size_t row, size_t col) const
+    {
+        if (row >= m_nrow || col >= m_ncol) {
+            throw std::invalid_argument("Dimension mismatch");
+        }
+        return m_buf[index(row, col)];
+    }
+
+    mat &operator=(const mat &m)
+    {
+        if (m_nrow != m.nrow() || m_ncol != m.ncol()) {
+            throw std::invalid_argument("Dimension mismatch");
+        }
+        if (&m != this) {
+            memcpy(m_buf, m.m_buf, sizeof(float) * m_nelem);
+        }
+        return *this;
+    }
+
+    mat &operator=(const std::vector<std::vector<float> > &m)
+    {
+        // FIXME: This function is only useful for unit-testing.
+        //        Could be useless in practice (No std::vector for embedded devices)
+        if (m_nrow != m.size() || m_ncol != m[0].size()) {
+            throw std::invalid_argument("Dimension mismatch");
+        }
+
+        // Assume to follow row major at the begining
+        for (size_t i = 0; i < m.size(); i++) {
+            memcpy(&m_buf[i * m_ncol], m[i].data(),
+                   sizeof(float) * m[i].size());
+        }
+        return *this;
+    }
+
 private:
+    size_t index(size_t row, size_t col) const
+    {
+        // TODO: Should we implement transpose illusion?
+        if (!false) {
+            return row * m_ncol + col;
+        } else {
+            return col * m_nrow + row;
+        }
+    }
+
     size_t m_nrow = R;
     size_t m_ncol = C;
     size_t m_nelem = R * C;
--- tests/TestMat.cpp
+++ tests/TestMat.cpp
@@ -10,3 +10,17 @@ TEST(Mat, Size)
     EXPECT_EQ(m1.ncol(), 4);
     EXPECT_EQ(m1.nelem(), 12);
 }
+
+TEST(Mat, Assign)
+{
+    picola::mat<2, 3> m1;
+    m1 = { { 1, 2, 3 }, { 4, 5, 6 } };
+
+    EXPECT_EQ(m1(0, 2), 3);
+    EXPECT_EQ(m1(1, 1), 5);
+    EXPECT_EQ(m1(1, 2), 6);
+
+    m1(0, 2) = 10;
+    EXPECT_EQ(m1(0, 2), 10);
+    EXPECT_NE(m1(1, 0), 10);
+}
